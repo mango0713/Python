545b75fea17f3629fd037c132e4055e0bdcedd87
mango0713 <72436232+mango0713@users.noreply.github.com>
2020-10-13 17:13:35 +0900

    Add files via upload

---
+++ 20191003-First.py
@@ -0,0 +1,51 @@
+print ("안녕하세요")
+print (" 저는 수내초등학교 5학년 1반 변서윤 입니다.")
+print (" 저는 천재 입니다.")
+print (" 나는 지난 2008년 생입니다.")
+
+mengo = 2000000000
+print (mengo)
+
+mengo =2019
+print (mengo)
+
+mengo1 = 300
+mengo2 = 500
+print (mengo1)
+print (mengo2)
+
+print (mengo1+mengo2)
+print (mengo1-mengo2)
+print (mengo1*mengo2)
+print (mengo1/mengo2)
+print (mengo1%mengo2)
+
+print (mengo1==mengo2)
+print (mengo1!= mengo2)
+print (mengo1>mengo2)
+print (mengo1<mengo2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
