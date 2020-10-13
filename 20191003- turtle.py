8c346bf30ac6e0a38974a4dcd97b8efa3985cdaf
mango0713 <72436232+mango0713@users.noreply.github.com>
2020-10-13 17:13:11 +0900

    Add files via upload

---
+++ 20191003- turtle.py
@@ -0,0 +1,35 @@
+import turtle as t
+
+t.shape("turtle")
+t. color("green")
+
+t. forward (90)
+t. left (90)
+t. forward (90)
+t. left (90)
+t. forward (90)
+t. left (90)
+t. forward (90)
+t. left(90)
+
+t .goto (200,200)
+t. color("red")
+
+t. forward (90)
+t. left (120)
+t. forward (90)
+t. left (120)
+t. forward (90)
+t. left (120)
+
+t. goto (-200,-200)
+t. color ("green")
+t.circle(200)
+
+
+ 
+
+
+
+
+
